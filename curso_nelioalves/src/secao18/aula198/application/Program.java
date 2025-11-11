e362b619fc8c932c59a3948459124540fa93ad94
Marina Barbosa Araujo <marina.ba@gmail.com>
2025-11-11 10:12:10 -0300

    expressão lambda inline

--- curso_nelioalves/src/secao18/aula198/application/Program.java
+++ curso_nelioalves/src/secao18/aula198/application/Program.java
@@ -24,9 +24,8 @@ public class Program {
 		
 		// List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
 		
-		Function<Product, String> func = p -> p.getName().toUpperCase();
 		
-		List<String> names = list.stream().map(func).collect(Collectors.toList()); 
+		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList()); 
 		
 		names.forEach(System.out::println);
 		
